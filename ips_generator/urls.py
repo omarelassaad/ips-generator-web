89c5ed2d86015481a4fa181414873fd2a7e9e48c
bardiabar <bardiabar@gmail.com>
2025-02-04 12:23:14 -0800

    feat: Add root URL redirect to login page

--- ips_generator/urls.py
+++ ips_generator/urls.py
@@ -4,8 +4,13 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from ips import views as ips_views
+from django.shortcuts import redirect
+
+def redirect_to_login(request):
+    return redirect('login')
 
 urlpatterns = [
+    path('', redirect_to_login, name='root'),  # Root URL redirects to login
     path('admin/', admin.site.urls),
     path('ips/', include('ips.urls')),  # Include URLs from the ips app
     path('accounts/login/', ips_views.login_view, name='login'),
